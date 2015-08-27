d3a1e9e8226de0e99fa49686ba69f326cca594a9
Ramsay Lanier <ramsay@nclud.com>
2015-08-27 15:16:36 -0400

    init

---
+++ package.js
@@ -0,0 +1,23 @@
+Package.describe({
+  name: 'ramsay:react-components-page',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'React Page component',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.3');
+
+  api.use([
+    'ramsay:react-components-dependencies@0.0.1'
+  ], 'client');
+
+  api.addFiles('component-page.jsx');
+
+  api.export(['Page', 'PageHeader', 'PageTitle', 'PageContent', 'PageSection', 'PageSeparator'], 'client');
+});
