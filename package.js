2e330aa427b589796111f4b3644ef065df2b9958
Ramsay Lanier <ramsay@nclud.com>
2015-08-27 15:29:26 -0400

    updated package git location

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'ramsay:react-components-page',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'React Page component',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/ramsaylanier/react-components-page',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -14,7 +14,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.1.0.3');
 
   api.use([
-    'ramsay:react-components-dependencies@0.0.1'
+    'ramsay:react-components-dependencies@0.0.2'
   ], 'client');
 
   api.addFiles('component-page.jsx');
